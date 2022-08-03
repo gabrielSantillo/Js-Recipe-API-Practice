b27aa7c5df3ac36b324b45cfdbfb03a5ea7015e3
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-03 11:59:23 -0600

    Trying to print the recipe for the chosen option

--- js/category.js
+++ js/category.js
@@ -24,10 +24,15 @@ function post_success(response) {
       `
         <button id="category_${
           response[`data`][`categories`][i][`strCategory`]
-        }">${response[`data`][`categories`][i][`strCategory`]}</button>
+        }">
+          ${response[`data`][`categories`][i][`strCategory`]}
+          </button>
         `
     );
   }
+
+  let button_Beef = document.getElementById(`category_Beef`);
+  button_Beef.addEventListener(`click`, showOptionsBeef);
 }
 
 function post_failure(error) {
@@ -41,24 +46,47 @@ axios
   .then(post_success)
   .catch(post_failure);
 
-function post_success_options(response) {
+function post_success_options_beef(response) {
+    let chosen_category = document.getElementById(`chosen_category`);
+    for(let i = 0; i < response[`data`][`meals`].length; i++) {
+        chosen_category.insertAdjacentHTML(`afterbegin`,
+        `
+        <div>
+        <a id="${response[`data`][`meals`][i][`idMeal`]}" href="#">${response[`data`][`meals`][i][`strMeal`]}</a>
+        <img src="${response[`data`][`meals`][i][`strMealThumb`]}">
+        </div>
+        `)
+    }
+
 
 }
 
-function post_failure_options(error) {
-    alert(`Sorry, chose your option again`)
+function post_failure_options_beef(error) {
+  alert(`Sorry, chose your option again`);
 }
 
-
-function showOptions(details) {
+function showOptionsBeef(details) {
   axios
     .request({
-      url: `https://www.themealdb.com/api/json/v1/1/filter.php?c=${
-        respose[`data`][i][`strCategory`]}`,
+      url: `https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef`,
     })
-    .then(post_success_options)
-    .catch(post_failure_options);
+    .then(post_success_options_beef)
+    .catch(post_failure_options_beef);
 }
 
-let button_Beef = document.getElementById(`category_Beef`);
-button_Beef.addEventListener(`click`, showOptions);
+function post_success_options_chicken(response) {
+    
+}
+
+function post_failure_options_chicken(error) {
+  alert(`Sorry, chose your option again`);
+}
+
+function showOptionsChicken(details) {
+    axios
+      .request({
+        url: `https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken`,
+      })
+      .then(post_success_options_chicken)
+      .catch(post_failure_options_chicken);
+  }
