37d94adb18fe33fdee170f201c413fcac3a1bf55
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-03 16:00:16 -0600

    Trying to make the category page works with all categories

--- js/category.js
+++ js/category.js
@@ -46,20 +46,50 @@ axios
   .then(post_success)
   .catch(post_failure);
 
+function post_success_recipe_zero(response) {
+    let chosen_category = document.getElementById(`chosen_category`);
+    chosen_category[`innerHTML`] = 
+    `
+        <div id="chosen_recipe">
+        <h2>${response[`data`][`meals`][0][`strMeal`]}</h2>
+        <img src="${response[`data`][`meals`][0][`strMealThumb`]}">
+        <p>${response[`data`][`meals`][0][`strInstructions`]}</p>
+        </div>
+    `
+}
+
+function post_failure_recipe_zero(error) {
+    alert(`Sorry, chose your option again`);
+}
+
+function showFirstRecipe(details) {
+  axios
+    .request({
+      url: `https://www.themealdb.com/api/json/v1/1/lookup.php?i=53000`,
+    })
+    .then(post_success_recipe_zero)
+    .catch(post_failure_recipe_zero);
+}
+
 function post_success_options_beef(response) {
     let chosen_category = document.getElementById(`chosen_category`);
-    for(let i = 0; i < response[`data`][`meals`].length; i++) {
-        chosen_category.insertAdjacentHTML(`afterbegin`,
+    for (let i = 0; i < response[`data`][`meals`].length; i++) {
+      chosen_category.insertAdjacentHTML(
+        `afterbegin`,
         `
-        <div>
-        <a id="${response[`data`][`meals`][i][`idMeal`]}" href="#">${response[`data`][`meals`][i][`strMeal`]}</a>
-        <img src="${response[`data`][`meals`][i][`strMealThumb`]}">
-        </div>
-        `)
+          <div class="recipes_card">
+          <a id="${response[`data`][`meals`][i][`idMeal`]}" href="#">${
+          response[`data`][`meals`][i][`strMeal`]
+        }</a>
+          <img src="${response[`data`][`meals`][i][`strMealThumb`]}">
+          </div>
+          `
+      );
     }
-
-
-}
+  
+    let link_receipt_zero = document.getElementById(`53000`);
+    link_receipt_zero.addEventListener(`click`, showFirstRecipe);
+  }
 
 function post_failure_options_beef(error) {
   alert(`Sorry, chose your option again`);
@@ -74,19 +104,17 @@ function showOptionsBeef(details) {
     .catch(post_failure_options_beef);
 }
 
-function post_success_options_chicken(response) {
-    
-}
+function post_success_options_chicken(response) {}
 
 function post_failure_options_chicken(error) {
   alert(`Sorry, chose your option again`);
 }
 
 function showOptionsChicken(details) {
-    axios
-      .request({
-        url: `https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken`,
-      })
-      .then(post_success_options_chicken)
-      .catch(post_failure_options_chicken);
-  }
+  axios
+    .request({
+      url: `https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken`,
+    })
+    .then(post_success_options_chicken)
+    .catch(post_failure_options_chicken);
+}
