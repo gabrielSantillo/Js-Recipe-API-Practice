0d345611aeb6d614d666df4d62780411155ec3bd
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-04 18:08:43 -0600

    still trying to solve how to show the meals for a chosen category and then the recipe for the chosen meal

--- js/category.js
+++ js/category.js
@@ -1,4 +1,18 @@
 function post_success(response) {
+  let chosen_category = document.getElementById(`chosen_category`);
+  for(let i = 0; i < response[`data`][`meals`].length; i++) {
+    chosen_category.insertAdjacentHTML(`afterbegin`, 
+    `
+    <div>
+      <h3 id="${response[`data`][`meals`][i][`idMeal`]}" value="${response[`data`][`meals`][i][`idMeal`]}">
+      ${response[`data`][`meals`][i][`strMeal`]}</h3>
+      <button id="${response[`data`][`meals`][i][`idMeal`]}" value="${response[`data`][`meals`][i][`idMeal`]}">View Recipe</button>
+    </div>
+    `
+    )
+  }
+
+  let button_id = document.getElementById(`${response[`data`][`meals`][0][`idMeal`]}`).value;
   
 }
 
